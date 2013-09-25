8fbebccd211db8daa9ed00e5cb4b8c0dab3dc3db
RU-019 <RU-019@Admins-Mac-mini-61.local>
2013-09-25 16:12:45 -0600

    first commit

---
+++ responsive.js
@@ -0,0 +1,20 @@
+
+/**
+XCreate a static HTML page based on the contents of this Wikipedia page.
+XInclude the main content in a container that takes up at most 850px.
+XAdd some padding to the container on devices smaller than 850px.
+XA sidebar that takes up 30% width but becomes no smaller than 320px. 
+XAdd a navigation bar that restructures itself into a more mobile-friendly design on smaller devices
+XAdd a couple fluid-width images, i.e. they adapt to the size of the page.
+XAdd an Image Gallery of Responsive Design examples that degrades into a vertical stack on smaller devices.
+*/
+
+$(document).ready(function() {
+	
+
+
+}); //don't remove. This closes $(document).ready
+
+
+
+ 
